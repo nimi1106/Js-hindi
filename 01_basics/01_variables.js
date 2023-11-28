02506e3aa1635df27282f411372efd3951503b53
nimi1106 <152277365+nimi1106@users.noreply.github.com>
2023-11-28 12:31:14 +0000

    understanding of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12344;
+let accountEmail = "nimi@gmail.com";
+var accountPassword = "12345";
+accountCity = "ahmdabad";
+
+// accountId = 2;// not allowedne
+
+accountEmail = "n@gmail.com";
+accountPassword = "0000";
+accountCity = " pune";
+let accountState;
+
+console.log(accountId);
+console.table([accountId ,accountEmail,accountPassword ,accountCity,accountState]);
+
+/*
+prefer not to use var
+*/
