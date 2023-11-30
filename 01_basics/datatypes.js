6f72004b57d333495261a34ac8cf3cffe0ac2e77
nimi1106 <152277365+nimi1106@users.noreply.github.com>
2023-11-30 13:50:21 +0530

    Create datatypes.js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+// *alert(3+3)*// we are using nodejs not browser
+
+let name="nimisha";  //*string datatype*//
+let age=36;  //*nmbr datatype*//
+let isloggedin=false;  //*boolean datatype*//
+
+// null=>stand alone value
+// undefined
+// symbol=>unique
+
+console.log(typeof"nimisha");
